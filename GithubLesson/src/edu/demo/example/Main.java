0636a7e1375e3dcd72dd97fc18e30949dfaecb36
Pawel Jasik <paweljasik93@gmail.com>
2015-04-22 20:25:42 +0200

    Initial commit

---
+++ GithubLesson/src/edu/demo/example/Main.java
@@ -0,0 +1,9 @@
+package edu.demo.example;
+
+public class Main {
+	public static void main(String args[]){
+		System.out.println("Hello, I'm Github.");
+		Other.hello();
+		//comment
+	}
+}
---
+++ GithubLesson/src/edu/demo/example/Other.java
@@ -0,0 +1,8 @@
+package edu.demo.example;
+
+public class Other {
+	public static void hello(){
+		System.out.println("Hello, I'm Other.");
+		//comment
+	}
+}
